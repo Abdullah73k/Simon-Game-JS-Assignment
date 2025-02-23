d968d37105be52bc02bf26773bba1d6a1679f5e6
Abdullah Khamis <abdullahbkhamis@gmail.com>
2025-02-23 11:17:16 -0500

    added some of the logic to comper arrays and proceed based on the comparison

--- JS/game.js
+++ JS/game.js
@@ -1,4 +1,4 @@
-import {nameInput, ageInput, colorInput} from "./userInput";
+import { nameInput, ageInput, colorInput } from "./userInput";
 
 let green = document.getElementById("green");
 let red = document.getElementById("red");
@@ -9,15 +9,21 @@ let help = document.getElementById("help");
 let modal = document.getElementById("modal");
 let close = document.getElementById("close-modal");
 
+let gameMessage = document.getElementById("game-state-text");
+
 // global variables
 let sequence = [];
 let isPlaying = false;
 let platerSequence = [];
+let playerWent = false;
+let round = 1;
 
+// displays the 'help' section
 help.onclick = () => {
 	modal.style.display = "flex";
 };
 
+// hides the 'help' section
 close.onclick = () => {
 	modal.style.display = "none";
 };
@@ -32,28 +38,61 @@ function randomColor() {
 
 // plays the random sequence created
 function playSequence() {
-    const redAudio = new Audio('red.ogg');
-    const blueAudio = new Audio('blue.way');
-    const yellowAudio = new Audio('yellow.way');
-    const greenAudio = new Audio('green.way');
+	const redAudio = new Audio("red.ogg");
+	const blueAudio = new Audio("blue.way");
+	const yellowAudio = new Audio("yellow.way");
+	const greenAudio = new Audio("green.way");
 
-
+	if (playerWent) { // only plays if player went
+		for (let btn of sequence) {
+			switch (btn) { // checks which color to click
+				case "red":
+					redAudio.play();
+					break;
+				case "blue":
+					blueAudio.play();
+					break;
+				case "green":
+					greenAudio.play();
+					break;
+				case "yellow":
+					yellowAudio.play();
+					break;
+			}
+		}
+	}
 }
 
 // gets the player input
 function playerInput() {
+    red.onclick = () => {
+        playerSequence.push('red');
+    }
+    blue.onclick = () => {
+        playerSequence.push('blue');
+    }
+    yellow.onclick = () => {
+        playerSequence.push('yellow');
+    }
+    green.onclick = () => {
+        playerSequence.push('green');
+    }
+}
 
+function checkPlayersSequence() {
+    if (playerWent) {
+        if (JSON.stringify(sequence) !== JSON.stringify(playSequence)) {
+            gameMessage.textContent = `You lost on round ${round}, click anywhere to play again!`;
+        }
+    }
 }
 
 // contains all of the other functions for the final game algorithm
 function gameAlgorithm() {
-	
-
-	while (!isPlaying) {
-        
-    }
+	while (!isPlaying) {}
 }
 
+// start game
 window.onclick = () => {
 	console.log("clicked");
 };
